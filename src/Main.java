87e6246d935384cfd62fa2fe7c7c6b1d97e90ef5
lyays <l28075339@gmail.com>
2025-03-02 20:31:10 +0300

    hm4

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,26 @@
 public class Main {
     public static void main(String[] args){
         //Задача 1
-        int age = 19;
-        boolean HeIsAnAdult = age >= 18;
-        if (HeIsAnAdult) {
+        int age = 17;
+        if (age >= 18) {
             System.out.println("Если возраст человека равен " + age + " он совершеннолетний");
         } else {
             System.out.println("Если возраст человека равен " + age + " он не достиг совершеннолетия, нужно немного подождать");
         }
         //Задача 2
+        int temperature = 6;
+        if (temperature < 5) {
+            System.out.println("На улице " + temperature + " градуса, нужно надеть шапку");
+        } else {
+            System.out.println("На улице " + temperature + " градусов, можно идти без шапки");
+        }
+        //Задача 3
+        int speed = 60;
+        if (speed <= 5) {
+            System.out.println("Если скорость " + speed + " км/ч, то придется заплатить штраф");
+        } else {
+            System.out.println("Если скорость " + speed + " км/ч, то можно ездить спокойно");
+
 
     }
 
