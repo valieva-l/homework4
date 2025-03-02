051261f85c2f751de1c42c8f073e1708f9ab1b09
lyays <l28075339@gmail.com>
2025-03-02 20:14:57 +0300

    hm4

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    public static void main(String[] args){
+        //Задача 1
+        int age = 19;
+        boolean HeIsAnAdult = age >= 18;
+        if (HeIsAnAdult) {
+            System.out.println("Если возраст человека равен " + age + " он совершеннолетний");
+        } else {
+            System.out.println("Если возраст человека равен " + age + " он не достиг совершеннолетия, нужно немного подождать");
+        }
+        //Задача 2
+
     }
+
 }
